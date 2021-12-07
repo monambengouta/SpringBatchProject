9977b0065501b0074748169aeb81383e5155a911
monambengouta <monam.bengouta@esprit.tn>
2021-12-07 21:59:55 +0100

    initialize envirment variable

---
+++ src/main/java/com/esprit/examen/services/IStockService.java
@@ -0,0 +1,11 @@
+package com.esprit.examen.services;
+
+import java.util.List;
+import com.esprit.examen.entities.Stock;
+
+public interface IStockService {
+
+
+	/*toDo 8*/
+
+}
---
+++ src/main/java/com/esprit/examen/services/StockServiceImpl.java
@@ -0,0 +1,22 @@
+package com.esprit.examen.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.esprit.examen.repositories.StockRepository;
+
+@Service
+public class StockServiceImpl implements IStockService {
+
+	@Autowired
+	StockRepository stockRepository;
+
+	/*toDo 9*/
+
+
+
+
+
+
+
+
+}
